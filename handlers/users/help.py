d26f6cbc378321941417d75accc3aca87ad89703
Mark <mabashin05@gmail.com>
2022-11-07 20:34:42 +0300

    .

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -5,4 +5,6 @@ from loader import dp
 @dp.message_handler(text='/help')
 async def command_help(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name}! \n'
-                         f'Тебе нужна помощь?')
+                         f'Тебе нужна помощь?\n'
+                         f'Могу помочь, но чуть позже.')
+
--- handlers/users/olimp.py
+++ handlers/users/olimp.py
@@ -1,7 +1,46 @@
 from aiogram import types
 from loader import dp
+from parse_olimp import parse_text
+
+subject_search = ([
+    'Биология', 'География', 'Информатика', 'Математика', 'Физика', 'Химия', 'Астрономия', 'ИЗО', 'Искусство',
+    'История', 'Лингвистика', 'Литература', 'ОБЖ', 'Обществознание', 'Предпринимательсво', 'Право',
+    'Психология', 'Робототехника', 'Русский язык', 'Технология', 'Физкультура', 'Черчение', 'Экология',
+    'Экономика', 'Ин.языки'
+])
+
+class_search = ([
+    str(i) for i in range(1, 12)
+])
+
+type_search = ([
+    'Командные', 'Личные', 'Дистанционные', 'Все'
+])
 
 
 @dp.message_handler(text='/olimp')
 async def command_olimp(message: types.Message):
-    await message.answer("Тут пока ничего нет")
+    await message.answer("Введите предмет:")
+
+
+
+@dp.message_handler(text=subject_search)
+async def answer1(message: types.Message):
+    global s
+    s = message.text.capitalize()
+    await message.answer("Введите класс:")
+
+
+@dp.message_handler(text=class_search)
+async def command_olimp(message: types.Message):
+    global c
+    c = message.text.capitalize()
+    await message.answer("Введите тип:")
+
+
+@dp.message_handler(text=type_search)
+async def command_olimp(message: types.Message):
+    global t
+    t = message.text.capitalize()
+    f = parse_text(s, c, t)
+    await message.answer(f)
